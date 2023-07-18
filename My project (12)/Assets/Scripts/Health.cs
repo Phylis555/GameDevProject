b0e9ef730d5ef9337c3447dae64c2cb225b5cef4
Phylis555 <phylis157@gmail.com>
2023-07-18 14:34:11 +0300

    ss

--- My project (12)/Assets/Scripts/Health.cs
+++ My project (12)/Assets/Scripts/Health.cs
@@ -1,31 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 
 public class Health : MonoBehaviour
 {
-    [SerializeField] private float startingHealth;
-    public float currentHealth { get; private set; }
+    //[SerializeField] private float startingHealth;
+    //public float currentHealth { get; private set; }
+
+    public int health;
+    public int numOfHearts;
+
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
     
     public Animator anim;
     private bool dead = false;
 
 
+    void Update()
+    {
 
+        if(health > numOfHearts)
+        {
+            health = numOfHearts;
+        }
+        for (int i = 0; i < hearts.Length; i++)
+        {
+
+            if(i < health)
+            {
+                hearts[i].sprite = fullHeart;
+
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+            if(i <numOfHearts)
+            {
+                hearts[i].enabled = true;
+            } else {
+                hearts[i].enabled = false;
+            }
+
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         Destroy(transform.GetComponent<Rigidbody>());
         if (other.tag == "Bear")
         {
-            if (!dead)
+
+            health--;
+            if (health <= 0)
             {
                 anim.SetTrigger("die");
                 GetComponent<playerMovmentScript>().enabled = false;
                 StartCoroutine(delay());
-                dead = true;
+               // dead = true;
 
                
 

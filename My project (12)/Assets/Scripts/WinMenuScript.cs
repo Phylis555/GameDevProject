176539a7cdf4fd5af41eb6c6611ba896a5895b61
LiatSagi <liat9171@gmail.com>
2023-07-20 16:46:25 +0300

    3

--- My project (12)/Assets/Scripts/WinMenuScript.cs
+++ My project (12)/Assets/Scripts/WinMenuScript.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 
 public class WinMenuScript : MonoBehaviour
 {
+    public Text numOfDimond;
     public void StartLevel()
     { //this function will be used on our Play button
         SceneManager.LoadScene(1);
@@ -15,7 +16,10 @@ public class WinMenuScript : MonoBehaviour
         SceneManager.LoadScene(0);
     }
 
- 
+    private void Start()
+    {
+        numOfDimond.text = scoreScript.score.ToString();
+    }
 
 
     public void OnEnable()

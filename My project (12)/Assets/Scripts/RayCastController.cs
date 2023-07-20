f739f706ea14fcb8676048f76b91f3b340d22af5
Phylis555 <phylis157@gmail.com>
2023-07-20 12:46:32 +0300

    k

--- My project (12)/Assets/Scripts/RayCastController.cs
+++ My project (12)/Assets/Scripts/RayCastController.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.AI;
 
 public class RayCastController : MonoBehaviour
 {
 
     private int layerMask;
     private int layerNumber = 6;
+
+    GameObject player;
+    NavMeshAgent agent;
+    [SerializeField] float sightRange;
+    bool playerInSight;
     //public float raycastDistance = 10f;
 
     //// Update is called once per frame
@@ -30,6 +36,8 @@ public class RayCastController : MonoBehaviour
 
     void Start()
     {
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.Find("Player");
         layerMask = 1 << layerNumber;
     }
     void Update()
@@ -39,6 +47,7 @@ public class RayCastController : MonoBehaviour
         Debug.DrawRay(raycastStartPosition, transform.forward * 5, Color.green);
         Vector3 raycastDirection = transform.forward;
 
+        
 
         // if (Physics.Raycast(raycastStartPosition, transform.forward, out hit, 15))
         if (Physics.Raycast(raycastStartPosition, transform.forward, out hit, 15, layerMask))
@@ -46,17 +55,23 @@ public class RayCastController : MonoBehaviour
             // Debug.Log("Hit");
             // if (hit.collider.gameObject.tag == "Player")
             // {
-            hit.collider.transform.position = new Vector3(719f, 18.8f, 503f);
-            transform.position = new Vector3(519f, -0.8f, 420f);
-            Debug.Log("inside: " + scoreScript.harts);
-            scoreScript.harts--;
-            if (scoreScript.harts <= 0)
-                SceneManager.LoadScene(3);
+            //hit.collider.transform.position = new Vector3(719f, 18.8f, 503f);
+            //transform.position = new Vector3(519f, -0.8f, 420f);
+            Chase();
+            //Debug.Log("inside: " + scoreScript.harts);
+            //scoreScript.harts--;
+            //if (scoreScript.harts <= 0)
+            //    SceneManager.LoadScene(3);
 
 
             // }
 
         }
+    }
+
+    void Chase()
+    {
+        agent.SetDestination(player.transform.position);
     }
 }
 

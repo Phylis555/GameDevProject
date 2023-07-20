2842e3b58ce0421b40b1bfab9e280d3a218fc2b2
LiatSagi <liat9171@gmail.com>
2023-07-20 21:56:00 +0300

    5

---
+++ My project (12)/Assets/Scripts/keyScript.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class keyScript : MonoBehaviour
+{
+    
+    public Text keyCollectText;
+    public Text cabinText;
+    public Text gardText;
+    [SerializeField] private AudioSource collectSound;
+
+    private void Start()
+    {
+        keyCollectText.enabled = false;
+        cabinText.enabled = false;
+        StartCoroutine(showgardTxt());
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag=="key")
+        {
+            collectSound.Play();
+            StartCoroutine(showKeyTxt());
+
+            cabinText.enabled = true;
+            scoreScript.score += 10;
+            scoreScript.keyColected = true;
+            other.gameObject.SetActive(false);
+        }
+      
+    }
+   
+
+    private IEnumerator showKeyTxt()
+    {
+        keyCollectText.enabled = true;
+        yield return new WaitForSeconds(5f);
+        keyCollectText.enabled = false;
+    }
+
+    private IEnumerator showgardTxt()
+    {
+        gardText.enabled = true;
+        yield return new WaitForSeconds(5f);
+        gardText.enabled = false;
+    }
+
+
+
+
+}

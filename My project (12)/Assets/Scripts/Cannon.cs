10c719be3e7ef39066bf5c544000a33bef6ef055
LiatSagi <liat9171@gmail.com>
2023-07-20 20:47:36 +0300

    5

--- My project (12)/Assets/Scripts/Cannon.cs
+++ My project (12)/Assets/Scripts/Cannon.cs
@@ -9,11 +9,13 @@ public class Cannon : MonoBehaviour {
 
     [SerializeField] private float velocity = 10;
 
+    [SerializeField] private AudioSource BoomSound;
 
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.G)&& scoreScript.inMaze) {
+            BoomSound.Play();
             var Nball = Instantiate(ball, ballSpawn.position, ballSpawn.rotation);
             Nball.Init(velocity);
         }
